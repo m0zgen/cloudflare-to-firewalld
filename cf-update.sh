77a2386f1fe21220efd85a51a395679aef32285e
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-01-14 18:30:40 +0600

    + firewall reload

--- cf-update.sh
+++ cf-update.sh
@@ -38,7 +38,8 @@ fi
 applyFirewall() {
     # Add IP addresses to zone
     echo "Update IPs in ${ZONE_NAME} firewalld zone..."
-    for i in `<${LISTS_CATALOG}/${1}`; do firewall-cmd --zone=${ZONE_NAME} --add-source=$i; done
+    for i in `<${LISTS_CATALOG}/${1}`; do firewall-cmd --zone=${ZONE_NAME} --add-source=$i --permanent; done
+    firewall-cmd --reload
 
 }
 
