2f9757add3811ce2097bd27d7f33c1abaf564141
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-01-21 21:59:15 +0600

    + calls & functions. Permanent argument.

--- cf-update.sh
+++ cf-update.sh
@@ -7,44 +7,122 @@
 # ---------------------------------------------------------------------\
 
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
-cd $SCRIPT_PATH
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd); cd $SCRIPT_PATH
+
+# Vars
+# ---------------------------------------------------------------------\
 
 ZONE_NAME=cloudflare
 LISTS_CATALOG=$SCRIPT_PATH/lists
 
+# Help information
+usage() {
+
+    echo -e "\nArguments:
+    -p (add ip addresses with --permanent)\n"
+    exit 1
+
+}
+
+# Checks arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -p|--permanent) _PERMANENT=1; ;;
+        -h|--help) usage ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 # Action
 # ---------------------------------------------------------------------\
 
-# Check cloudflare zone exists
-echo "Checking zone ${ZONE_NAME} exists in"
-_CF_ZONE_STATUS=$(firewall-cmd --list-all --zone=${ZONE_NAME} | head -1)
+check_zone_exists() {
 
-if [[ "${_CF_ZONE_STATUS}" =~ "${ZONE_NAME}" ]]; then
-    echo "Zone ${ZONE_NAME} Exist"
-else
-    echo "Need to add zone ${ZONE_NAME}"
-    firewall-cmd --new-zone=${ZONE_NAME} --permanent
-    # firewall-cmd --zone=${ZONE_NAME} --add-service={http, https} --permanent
-    firewall-cmd --reload
-fi
+    # Check cloudflare zone exists
+    echo -e "\nChecking zone ${ZONE_NAME} exists in"
+    _CF_ZONE_STATUS=$(firewall-cmd --list-all --zone=${ZONE_NAME} | head -1) 
 
-# Check catalog for downloaded ip ranges lists
-if [[ ! -d "${LISTS_CATALOG}" ]]; then
-    mkdir -p ${LISTS_CATALOG}
-fi
+    if [[ "${_CF_ZONE_STATUS}" =~ "${ZONE_NAME}" ]]; then
+        echo "Zone ${ZONE_NAME} Exist"
+    else
+        echo "Zone does not found. Adding new zone: ${ZONE_NAME}"
+        firewall-cmd --new-zone=${ZONE_NAME} --permanent
+        # firewall-cmd --zone=${ZONE_NAME} --add-service={http, https} --permanent
+        firewall-cmd --reload
+        echo "New zone: ${ZONE_NAME} added."
+    fi
+}
 
-# Download lists
-echo "Downloading IPv4 list..."
-curl -sS https://www.cloudflare.com/ips-v4 > ${LISTS_CATALOG}/ips.txt
-echo -e "\n" >> ${LISTS_CATALOG}/ips.txt
-echo "Downloading IPv6 list..."
-curl -sS https://www.cloudflare.com/ips-v6 >> ${LISTS_CATALOG}/ips.txt
+check_list_catalog() {
 
-# Add IP addresses to zone
-echo "Update IPs in ${ZONE_NAME} firewalld zone..."
-for i in `<${LISTS_CATALOG}/ips.txt`; do firewall-cmd --zone=${ZONE_NAME} --add-source=$i; done
+    # Check catalog for downloaded ip ranges lists
+    if [[ ! -d "${LISTS_CATALOG}" ]]; then
+        mkdir -p ${LISTS_CATALOG}
+    fi
+}
 
-firewall-cmd --zone=${ZONE_NAME} --add-service=https
+download_lists() {
 
-echo "Done!"
+    # Download lists
+    echo -e "\nDownload Cloudflare IP Ranges lists..."
+    echo "Downloading IPv4 list..."
+    curl -sS https://www.cloudflare.com/ips-v4 > ${LISTS_CATALOG}/ips.txt
+    echo -e "\n" >> ${LISTS_CATALOG}/ips.txt
+    echo "Downloading IPv6 list..."
+    curl -sS https://www.cloudflare.com/ips-v6 >> ${LISTS_CATALOG}/ips.txt
+}
+
+push_to_firewalld() {
+    
+    if [[ -z "${1}" ]]; then
+        # Add IP addresses to zone
+        echo -e "\nUpdate IPs in ${ZONE_NAME} firewalld zone..."
+    else
+        echo -e "\nUpdate IPs in ${ZONE_NAME} firewalld zone... With permanent parameter."
+    fi
+    
+    for i in `<${LISTS_CATALOG}/ips.txt`; do firewall-cmd --zone=${ZONE_NAME} --add-source=$i ${1}; done
+
+    if [[ -z "${1}" ]]; then
+        echo -e "\nAdd HTTPS protocol to zone..."
+    else
+        echo -e "\nAdd HTTPS protocol to zone... With permanent parameter."
+    fi
+    
+    firewall-cmd --zone=${ZONE_NAME} --add-service=https ${1}
+
+    if [[ ! -z "${1}" ]]; then
+        echo -e "\nApplying settings, firewalld will be reloaded..."
+        firewall-cmd --reload
+        
+    fi
+}
+
+add_ip_to_zone() {
+
+    if [[ "$_PERMANENT" -eq "1" ]]; then
+        push_to_firewalld "--permanent"
+    else
+        push_to_firewalld
+    fi
+}
+
+finality() {
+
+    echo -e "\nDone!\n"
+    echo -e "Note: You can review firewalld ${ZONE_NAME} with command:
+    firewall-cmd --list-all --zone=cloudflare\n"
+}
+
+# Calls
+# ---------------------------------------------------------------------\
+check_zone_exists
+check_list_catalog
+download_lists
+add_ip_to_zone
+finality
+
+
+
+
